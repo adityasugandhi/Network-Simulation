c3ca162e1ac0d9dcbd47ea1a648f8b86aaec021e
adityasugandhi <39901010+adityasugandhi@users.noreply.github.com>
2023-11-20 13:28:43 -0500

    Created Lanhooks class to connect to bridge

--- utils/station_utils/lan_hooks.py
+++ utils/station_utils/lan_hooks.py
@@ -4,24 +4,28 @@ from utils.bridge_utils.bridge_parser import Bridgeparser
 
 B = Bridgeparser()
 
-def connect_to_bridge(ip_address, port, macaddress):
-    try:
-        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print('------Connecting to server----')
-        s.connect((ip_address, port))
-        # s.send(macaddress.encode())
-        print('-----Waiting for response-----')
-        response = s.recv(1024).decode()
-        if response == 'accept':
-            print('-----Connection accepted-----')
-            s.send(macaddress.encode())
-        return response, s
-    except socket.error as e:
-        print(f"Error connecting to {ip_address}:{port}: {e}")
-        return "reject"
+class Lanhooks:
+    def __init__(self) -> None:
+        self.BRIDGE_FILE_PATH = 'utils/station_utils/bridge.txt'
+        self.bridges = []
+    @staticmethod
+    def connect_to_bridge(ip_address, port, macaddress):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                print('------Connecting to server----')
+                s.connect((ip_address, port))
+                s.send(macaddress.encode())
+                print('-----Waiting for response-----')
+                response = s.recv(1024).decode()
+                if response == 'accept':
+                    print('-----Connection accepted-----')
+                    s.send(macaddress.encode())
+                return response, s
+        except socket.error as e:
+            print(f"Error connecting to {ip_address}:{port}: {e}")
+            return "reject"
 
-
+lhooks = Lanhooks()
 def connect_to_all_lans(interfaces):
     connections = []
     for interface in interfaces:
@@ -41,7 +45,7 @@ def connect_to_all_lans(interfaces):
                 print(f"Bridge {interface_dict['Lan Name']} not found")
             if port:
                 # Connect to the bridge
-                response,s = connect_to_bridge(bridge.ip_address,port, interface_dict['Mac Address'])
+                response,s = lhooks.connect_to_bridge(bridge.ip_address,port, interface_dict['Mac Address'])
 
                 if response == "accept":
                     print(f"Connected to {bridge.name} bridge at {bridge.ip_address}:{bridge.port}")
